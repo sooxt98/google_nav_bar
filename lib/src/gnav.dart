89c219daa40715791a3ca6acf4640b13297bf133
Jagandeep Brar <me@jagandeepbrar.io>
2021-03-11 04:12:10 -0600

    Use didUpdateWidget to set selectedIndex when rebuilding

--- lib/src/gnav.dart
+++ lib/src/gnav.dart
@@ -71,6 +71,12 @@ class _GNavState extends State<GNav> {
   }
 
   @override
+  void didUpdateWidget(GNav oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if(widget.selectedIndex != oldWidget.selectedIndex) selectedIndex = widget.selectedIndex;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
         color: widget.backgroundColor,
